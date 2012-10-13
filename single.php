8d9b65a4c798466bac91b9f6f149dd4ce96846f0
Kenth Söderholm <kenth.south@gmail.com>
2012-10-13 13:59:18 +0200

    Added db description
    Added database and table description
    created single.php

---
+++ single.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Utvisning stora bilder</title>
+		<link href="mall.css" rel="stylesheet" type="text/css">
+	</head>
+	
+	<body>
+	
+	</body>
+</html>
