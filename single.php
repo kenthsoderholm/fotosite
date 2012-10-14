6e20c1ec53b314964ed3f6a2f6eb1e7ae53a7962
Kenth Söderholm <kenth.south@gmail.com>
2012-10-14 12:40:34 +0200

    Added single.php
    Added single.php
    Now it outputs both the watermarked and the original picture, after
    implementing users and login it will show one of the pictures depending
    on if the user is logged in or not.

--- single.php
+++ single.php
@@ -1,5 +1,11 @@
 <?php
-
+	require_once('includes/conn.php');
+	if (!isset($_GET['id']) || $_GET['id'] <= 0) {
+		header('location: index.php');
+	}
+	else {
+		$id = (int) $_GET['id'];
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,6 +16,31 @@
 	</head>
 	
 	<body>
-	
+		<div id="wrapper">
+			<div id="header"><h1>Fotosite by Kenth</h1></div>
+			<div id="menu">
+				<a href="index.php">Startsidan</a> | <a href="backend.php">Backend</a>
+			</div>
+			
+			<div id="content">
+				<?php
+					$conn = mysqli_connect($hostname, $username, $password, $database);
+					if (mysqli_connect_errno()) {
+						echo "Fel: ".mysqli_connect_errno();
+					}
+					$query = "select imageLink, imageWatermarkLink from images_v1 where imageID = '$id'";
+					$result = mysqli_query($conn, $query);
+					while ($row = mysqli_fetch_assoc($result)) {
+						echo '
+							<p>För inloggade ska denna visas: </p><img src="'.$row['imageLink'].'"> <br><p>För andra ska denna visas: </p> 
+							<img src="'.$row['imageWatermarkLink'].'">
+						';
+					} //Now it shows both watermarked and original image, will change this after adding login
+				?>
+			</div>
+			
+			<div id="footer"></div>
+		</div>
+
 	</body>
 </html>
